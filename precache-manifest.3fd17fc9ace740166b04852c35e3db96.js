0e298b2fcedba2b5f8a4d74c7611428ad3f0fff6
Guilherme Barros <barros.guilherme21@gmail.com>
2022-05-04 19:05:09 -0700

    Updates

---
+++ precache-manifest.3fd17fc9ace740166b04852c35e3db96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6feca325d7c22913f25e1bcf15258eb8",
+    "url": "/hulu-clone/index.html"
+  },
+  {
+    "revision": "e7a953101120f6cd2eda",
+    "url": "/hulu-clone/static/css/main.f5330124.chunk.css"
+  },
+  {
+    "revision": "796c3c6ed4f76b48dfc4",
+    "url": "/hulu-clone/static/js/2.af4190a7.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/hulu-clone/static/js/2.af4190a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a953101120f6cd2eda",
+    "url": "/hulu-clone/static/js/main.56740e61.chunk.js"
+  },
+  {
+    "revision": "0f44b8927cd95b226457",
+    "url": "/hulu-clone/static/js/runtime-main.df278e7f.js"
+  }
+]);
